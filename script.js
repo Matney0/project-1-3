509475e457493532fb1a7ea571a0fe94608e0e74
Matney0 <mlmoller21@amarillocollege.com>
2025-03-20 13:37:29 -0500

    feeeee

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function myFunction() {
   document.getElementById("submitbutton").innerHTML = "Thank you for your order!";
 }
+let submitButton = document.getElementById("submitbutton");
+submitButton.addEventListener("click", myFunction);
