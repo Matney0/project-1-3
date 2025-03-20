0cc9c7694c6d3936597804158151fddeb3ca8d71
Matney0 <mlmoller21@amarillocollege.com>
2025-03-20 13:05:23 -0500

    dewe

---
+++ script.js
@@ -0,0 +1,7 @@
+<script>
+   
+
+let submitbutton = document.getElementById('submitbutton');
+submitbutton.addEventListener('click', function() {
+document.write('Thank you for your order!')};    
+</script>
